d9100ae33eb9a0e725f59c6310ac89bdcdf5c6ab
slawoe <info@slawomirernst.de>
2021-02-21 08:33:25 +0100

    Add fontAweseome Icons to Player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Player = () => {
   return (
     <div className="player">
@@ -7,6 +14,9 @@ const Player = () => {
         <span>End Time</span>
       </div>
       <div className="play-control"></div>
+      <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+      <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+      <FontAwesomeIcon className="skip-forward" icon={faAngleRight} size="2x" />
     </div>
   );
 };
