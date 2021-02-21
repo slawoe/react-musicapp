04d8bd7615ccdc8ea8730cc7854c487b93ed0d1d
slawoe <info@slawomirernst.de>
2021-02-21 11:42:02 +0100

    Add isPlaying State to App, drill it to player and implement the play/pause in playSongHandler

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ import data from "./util";
 function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
     </div>
   );
 }
