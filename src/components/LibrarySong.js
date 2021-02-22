e4ccfdc3a132d763e14172854279d68761bb8886
slawoe <info@slawomirernst.de>
2021-02-22 13:56:59 +0100

    Add div.song-description for styling reasons to LibrarySong

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -2,8 +2,10 @@ const LibrarySong = ({ song }) => {
   return (
     <div className="library-song-container">
       <img src={song.cover} alt={`Song cover of ${song.name}`} />
-      <h2>{song.name}</h2>
-      <span>{song.artist}</span>
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <span>{song.artist}</span>
+      </div>
     </div>
   );
 };
